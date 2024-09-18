20a9d6180289859aecafaee9adffa4fa2e55a43a
WarAndRoses <109928835+WarAndRoses@users.noreply.github.com>
2024-09-18 01:02:05 -0300

    falta estilo

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,7 +1,55 @@
-let cuatro = document.getElementById("cuatro");
+let resultado = document.getElementById("pantalla");
+let actualOperation = "";
+let hopeOperation = "";
 
-cuatro.addEventListener("click", function() {
-    ;
-});
+function addNum(numero){
+    actualOperation += numero;
+    resultado.value = actualOperation;
+}
 
-console.log(cuatro);
+function operation (op) {
+    if (actualOperation === "") return;
+    if (hopeOperation !== "") {
+        calcule();
+    }
+    hopeOperation = actualOperation + ' ' + op;
+    actualOperation = "";
+}
+
+function calcule() {
+    let calculo;
+    const partes = hopeOperation.split(" ");
+    const num1 = parseFloat(partes[0]);
+    const num2 = parseFloat(actualOperation);
+    const operador = partes[1];
+
+
+    if (isNaN(num1) || isNaN(num2))  return; 
+
+        switch(operador){
+            case '+':
+                calculo = num1 + num2;
+                break;
+
+            case '-':
+                calculo = num1 - num2;
+                break;
+            case '/':
+                calculo = num1 / num2;
+                break;
+            case '*':
+                calculo = num1 * num2;
+                break;            
+        }
+
+       resultado.value = calculo;
+       actualOperation = "";
+       hopeOperation = "";
+    
+}
+
+function limpiar() {
+    actualOperation = "";
+    hopeOperation = "";
+    resultado.value = "";
+}
